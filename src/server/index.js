22d8c19243388a47925376d376a8ada00593a7a3
Vegard Bjerkli Bugge <vegabug@live.no>
2020-08-07 15:46:10 +0200

    Add Svelte boilerplate source code

---
+++ src/server/index.js
@@ -0,0 +1,8 @@
+/**
+Main module for the backend code
+*/
+console.log('TODOS')
+console.log('Install AVA Test Suite: --save-dev')
+console.log('Install dotenv for configuring database variables and a salt string for the user manager')
+console.log('Install ESLint using your preferred work config --save-dev')
+console.log('Define a Node based user handling module using pbkbdf2 for hashing and salting passwords')
