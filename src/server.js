e9290b497fb5b9f3b01fad917d7549503f8eeb35
Vegard Bjerkli Bugge <vegabug@live.no>
2020-08-08 18:05:27 +0200

    Provide argument to closeCOnnection

--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
-import appFactory from './server/appFactory'
 import http from 'http'
+import appFactory from './server/appFactory'
+import instantiateDB from './server/database'
+import closeConnection from './server/database/closeConnection'
 
 const app = appFactory(process.env)
+const connection = instantiateDB(process.env)
 const server = http.createServer(app)
 const { PORT } = process.env
 server.listen(
@@ -16,6 +19,6 @@ server.listen(
 // node -r dotenv/config dotenv_config_path=/secret.env src/server.js
 
 process.on('SIGINT', () => {
-  closeConnection()
+  closeConnection(connection)
   process.exit(0)
 })
