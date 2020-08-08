d3afbd06e9e1d7591728390d03515eecbee83efc
Vegard Bjerkli Bugge <vegabug@live.no>
2020-08-08 14:18:44 +0200

    Use ES in appFactory

--- src/server/appFactory.js
+++ src/server/appFactory.js
@@ -1,12 +1,12 @@
-const express = require('express')
-const helmet = require('helmet') // todo: install
+import express from 'express'
+import helmet from 'helmet'
 
 // todo: import connection-factory
 // todo: server file
 // todo: Use ES imports and Webpack config
 
-module.exports = function appFactory(processEnv) {
+export default function appFactory(processEnv) {
   const app = express()
   app.use(helmet())
   return app
-};
+}
