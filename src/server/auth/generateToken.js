4bf17583f3123aa290083610709c9992993ad372
Vegard Bjerkli Bugge <vegabug@live.no>
2020-08-08 16:14:34 +0200

    Convert to ES module

--- src/server/auth/generateToken.js
+++ src/server/auth/generateToken.js
@@ -1,39 +1,41 @@
-const { randomBytes } = require('crypto');
+import { randomBytes } from 'crypto'
 
 /**
  * @function getRandomLetter
  * Generates a random letter amongst the ones present in the string of url-safe characters
  * @return {String} A letter to replace either a + or /
  */
-const getRandomLetter = () => '5vWcDd8EeZzF6fGgHrS3sTthI0iJajP4pQqRm_NnOo9K-k7LlMwX2xYyABbCU1uV'.split('')[Math.floor(Math.random() * 64)];
+const getRandomLetter = () => '5vWcDd8EeZzF6fGgHrS3sTthI0iJajP4pQqRm_NnOo9K-k7LlMwX2xYyABbCU1uV'.split('')[Math.floor(Math.random() * 64)]
 
 /**
  * Generates a string of length len
- * If the function randomBytes fails, use getRandomLetter to generator one new letter for each index
+ * If the function randomBytes fails, use getRandomLetter
+ * to generate one new letter for each index
  * @param {Number} len - Indicates the string size, denoted in the amount of characters,
  * to be generated in the token
  * @param {Boolean} url - Flag indicating whether the string must be url-safe
  * @return {String} The string to be used as either a secret or in URL
  */
-module.exports = function generateToken(len, url = false) {
-  // Get the number of bytes to generate. Each Base64 digit represents exactly 6 bits of data
-  const x = Math.ceil((len * 3) / 4);
-  let token = '';
+export default function generateToken(len, url = false) {
+  // Get the number of bytes to generate.
+  // Each Base64 digit represents exactly 6 bits of data
+  const x = Math.ceil((len * 3) / 4)
+  let token = ''
   try {
-    token = randomBytes(x).toString('base64');
+    token = randomBytes(x).toString('base64')
     if (url) {
-      token = token.split('/').join('').split('+').join('');
+      token = token.split('/').join('').split('+').join('')
     }
     if (token.length > len) {
-      token = token.substring(0, len);
+      token = token.substring(0, len)
     }
     while (token.length < len) {
-      token = `${getRandomLetter()}${token}`;
+      token = `${getRandomLetter()}${token}`
     }
   } catch (e) {
     while (token.length < len) {
-      token = `${token}${getRandomLetter()}`;
+      token = `${token}${getRandomLetter()}`
     }
   }
-  return token;
-};
+  return token
+}
