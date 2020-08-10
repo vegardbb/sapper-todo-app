a07f176cd0b6fa102e8e22c5fc71326dddc83aad
Vegard Bjerkli Bugge <vegabug@live.no>
2020-08-10 02:22:41 +0200

    Two new components

---
+++ src/components/Login.svelte
@@ -0,0 +1,42 @@
+<script>
+    let error_boolean = false;
+
+   async function handleSubmit(event) {
+       console.log(event);
+       console.log(event.target);
+       console.log(event.target.email.value);
+       console.log(event.target.password.value);
+    }
+
+   function validateMessageEmail(event) {
+       let textbox = event.target;
+       error_boolean = false;
+       if (textbox.value === '') {
+            textbox.setCustomValidity('Required email address');
+        } else if (textbox.validity.typeMismatch){
+            error_boolean = true;
+            textbox.setCustomValidity('please enter a valid email address');
+        } else {
+           textbox.setCustomValidity('');
+        }
+        return true;
+    }
+</script>
+
+<form
+    on:submit|preventDefault="{handleSubmit}"
+    on:invalid={validateMessageEmail}
+    on:changed={validateMessageEmail}
+    on:input={validateMessageEmail}
+>
+  <label for="email">Email</label>
+  <input required type="email" id="email" />
+  {#if error_boolean}
+    <h1> OH NO! AN ERRROR!</h1>
+  {/if}
+
+  <label for="password">Password</label>
+  <input required type="password" id="password" />
+
+  <button type="submit">Create account</button>
+</form>
---
+++ src/components/Signup.svelte
@@ -0,0 +1,38 @@
+<script>
+  // let error_boolean = false;
+
+  async function handleSubmit(event) {
+    console.log(event);
+    console.log(event.target);
+    console.log(event.target.username.value);
+    console.log(event.target.password.value);
+  }
+
+  function validateName(event) {
+    const textbox = event.target;
+    // error_boolean = false;
+    if (textbox.value === '') {
+      textbox.setCustomValidity('A user name with at least one letter is required');
+    } else {
+      textbox.setCustomValidity('');
+    }
+    return true;
+  }
+</script>
+
+<form
+  on:submit|preventDefault="{handleSubmit}"
+  on:invalid={validateName}
+  on:changed={validateName}
+  on:input={validateName}
+>
+  <label for="username">User name</label>
+  <input required id="username" />
+  {#if error_boolean}
+    <h1> OH NOES, AN ERRROR OCCURRED!</h1>
+  {/if}
+  <label for="password">Your Password</label>
+  <input required type="password" id="password" />
+
+  <button type="submit">Create account</button>
+</form>
