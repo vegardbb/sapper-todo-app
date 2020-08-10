6ae3b2fd4e9d3022304a32726152ccbe6942f1b7
Vegard Bjerkli Bugge <vegabug@live.no>
2020-08-10 02:23:06 +0200

    Add todo route and user route

---
+++ src/routes/todo.js
@@ -0,0 +1,51 @@
+import getDB from '../server/database'
+import getDateTime from '../server/getDateString'
+import { createTodo, deleteTodo, editTodo, getUserTodos } from '../server/queries'
+import generateToken from '../server/auth/generateToken'
+
+export async function post(req, res) {
+  try {
+    const { userName } = req.session
+    const { todoText } = req.body
+    const id = generateToken(96, null)
+    const date = getDateTime(new Date().toISOString())
+    await createTodo(getDB(), id, todoText, date, userName)
+    res.end(JSON.stringify({ success: true }))
+  } catch (error) {
+    res.end(JSON.stringify({ error: error.message }))
+  }
+}
+
+export async function del(req, res) {
+  try {
+    const { userName } = req.session
+    const { todoID } = req.body
+    const id = generateToken(96, null)
+    const date = getDateTime(new Date().toISOString())
+    await deleteTodo(getDB(), todoID, userName)
+    res.end(JSON.stringify({ success: true }))
+  } catch (error) {
+    res.end(JSON.stringify({ error: error.message }))
+  }
+}
+
+export async function patch(req, res) {
+  try {
+    const { userName } = req.session
+    const { todoID, todoText } = req.body
+    await editTodo(getDB(), todoID, todoText, userName)
+    res.end(JSON.stringify({ success: true }))
+  } catch (error) {
+    res.end(JSON.stringify({ error: error.message }))
+  }
+}
+
+export async function get(req, res) {
+  try {
+    const { userName } = req.session
+    const todos = await getUserTodos(getDB(), userName)
+    res.end(JSON.stringify({ success: true, todos }))
+  } catch (error) {
+    res.end(JSON.stringify({ error: error.message }))
+  }
+}
---
+++ src/routes/user.js
@@ -0,0 +1,24 @@
+// POST route for creating a new user
+import { promisify } from 'util'
+import { generateSalt, generateHash } from '../server/auth/salt'
+import getDB from '../server/database'
+import { createUser } from '../server/queries'
+
+export async function post(req, res) {
+  try {
+    const { username, password, firstName, lastName } = req.body
+    const saveSession = promisify(req.session.save).bind(req.session)
+    // generate hash and salt
+    const salt = generateSalt()
+    const hash = await generateHash(password, salt)
+    await createUser(getDB(), username, lastName, firstName, salt, hash)
+    req.session.firstName = firstName
+    req.session.lastName = lastName
+    req.session.userName = username
+    const sess = { ...req.session }
+    await saveSession()
+    res.end(JSON.stringify(sess))
+  } catch (error) {
+    res.end(JSON.stringify({ loginRequired: true, error: error.message }))
+  }
+}
