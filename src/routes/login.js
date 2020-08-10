ab90e224fad764de967297ab90e942adda21c80e
Vegard Bjerkli Bugge <vegabug@live.no>
2020-08-10 02:21:55 +0200

    Add server login route

---
+++ src/routes/login.js
@@ -0,0 +1,27 @@
+import { promisify } from 'util'
+import { verifyHash } from '../server/auth/salt'
+import getDB from '../server/database'
+import { getUserByID } from '../server/queries'
+
+export async function post(req, res) {
+  try {
+    const { username, password } = req.body
+    const saveSession = promisify(req.session.save).bind(req.session)
+    const userRow = await getUserByID(getDB(), username)
+    const { salt, bloat } = userRow
+    const isValidSession = await verifyHash(password, bloat, salt)
+    if (isValidSession) {
+      const { familyName, christianName } = userRow
+      req.session.firstName = christianName
+      req.session.lastName = familyName
+      req.session.userName = username
+      const sess = { ...req.session }
+      await saveSession()
+      res.end(JSON.stringify(sess))
+    } else {
+      res.end(JSON.stringify({ loginRequired: true, error: 'Invalid password' }))
+    }
+  } catch (error) {
+    res.end(JSON.stringify({ loginRequired: true, error: error.message }))
+  }
+}
