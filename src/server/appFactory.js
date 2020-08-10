dd1b1c4958b25027280a9361d3127c05ac8146f6
Vegard Bjerkli Bugge <vegabug@live.no>
2020-08-10 02:19:05 +0200

    Use JSON parser; use session handler

--- src/server/appFactory.js
+++ src/server/appFactory.js
@@ -1,20 +1,42 @@
 import express from 'express'
+import session from 'express-session'
 import helmet from 'helmet'
 import sirv from 'sirv'
 import compression from 'compression'
+import { json } from 'body-parser'
+// eslint-disable-next-line import/no-extraneous-dependencies
 import * as sapper from '@sapper/server'
-
-// todo: import connection-factory
-// todo: server file
-// todo: Use ES imports and Webpack config
+import SqliteStore from './sessionStore'
+import exists from './objectExists'
 
 export default function appFactory(processEnv) {
-  const { NODE_ENV } = processEnv
+  const { NODE_ENV, SECRET } = processEnv
   const dev = NODE_ENV === 'development'
   const app = express()
+  // app.set('trust proxy', 1) // trust first proxy, enable in production
   app.use(helmet())
+  app.use(json())
+  app.use(session({
+    secret: SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 2764800000 },
+    store: new SqliteStore(processEnv)
+  }))
   app.use(compression({ threshold: 0 }))
   app.use(sirv('static', { dev }))
-  app.use(sapper.middleware())
+  app.use(sapper.middleware({
+    session: req => {
+      const { session: s } = req
+      if (exists(s)) {
+        const { christianName, familyName } = s
+        return {
+          firstName: typeof christianName === 'string' ? christianName : null,
+          lastName: typeof familyName === 'string' ? familyName : null
+        }
+      }
+      return { loginRequired: true, firstName: null, lastName: null }
+    }
+  }))
   return app
 }
