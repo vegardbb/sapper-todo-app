85c96705d2189ea6c7a441fe42946aa48479c5d4
Vegard Bjerkli Bugge <vegabug@live.no>
2020-08-22 13:41:02 +0200

    Refactoring to read the result from an array

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/prefer-default-export */
 import { promisify } from 'util'
 import { verifyHash } from '../server/auth'
 import getDB from '../server/database'
@@ -8,10 +9,9 @@ export async function post(req, res) {
     const { username, password } = req.body
     const saveSession = promisify(req.session.save).bind(req.session)
     const userRow = await getUserByID(getDB(), username)
-    const { salt, bloat } = userRow
+    const { salt, bloat, familyName, christianName } = userRow[0]
     const isValidSession = await verifyHash(password, bloat, salt)
     if (isValidSession) {
-      const { familyName, christianName } = userRow
       req.session.firstName = christianName
       req.session.lastName = familyName
       req.session.userName = username
