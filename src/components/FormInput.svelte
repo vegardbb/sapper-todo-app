cc0115135690a0c8ee963d8df82d90d5abe8ce2d
Vegard Bjerkli Bugge <vegabug@live.no>
2020-08-26 08:11:08 +0200

    Define form component for signup and login

---
+++ src/components/FormInput.svelte
@@ -0,0 +1,27 @@
+<script>
+  export let inputID = form-input
+  export let boldText = 'Form Input'
+  export let inField = 'Write text'
+  export let required = false
+</script>
+
+<style>
+/* Full-width input fields */
+input[type=text], input[type=password] {
+  width: 100%;
+  padding: 15px;
+  margin: 5px 0 22px 0;
+  display: inline-block;
+  border: none;
+  background: #f1f1f1;
+}
+
+/* Add a background color when the inputs get focus */
+input[type=text]:focus, input[type=password]:focus {
+  background-color: #ddd;
+  outline: none;
+}
+</style>
+
+<label for="{inputID}"><b>{boldText}</b></label>
+<input type="text" placeholder="{inField}" name="{inputID}" {required}>
